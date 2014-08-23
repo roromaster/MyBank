0990e087b69dc6c0f734ebb2558eff3d2870fb7c
Roro <roromaster@gmail.com>
2014-08-23 21:02:09 +0200

    Register

---
+++ eTIS-Bank/faceRegisterVC.h
@@ -0,0 +1,13 @@
+//
+//  faceRegisterVC.h
+//  eTIS-Bank
+//
+//  Created by Rodolphe on 23/08/2014.
+//  Copyright (c) 2014 Rodolphe Hugel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface faceRegisterVC : UIViewController
+
+@end
